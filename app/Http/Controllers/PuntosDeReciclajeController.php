5123142a67bc6b151e0d9874e667221f9a5d9a12
fernandoDC <fernando@dcircuits.mx>
2020-10-14 18:29:01 -0500

    Controlador de puntos de reciclaje agregado

---
+++ app/Http/Controllers/PuntosDeReciclajeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PuntoDeReciclaje;
+use Illuminate\Http\Request;
+
+class PuntosDeReciclajeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PuntoDeReciclaje  $puntoDeReciclaje
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PuntoDeReciclaje $puntoDeReciclaje)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PuntoDeReciclaje  $puntoDeReciclaje
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PuntoDeReciclaje $puntoDeReciclaje)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PuntoDeReciclaje  $puntoDeReciclaje
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PuntoDeReciclaje $puntoDeReciclaje)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PuntoDeReciclaje  $puntoDeReciclaje
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PuntoDeReciclaje $puntoDeReciclaje)
+    {
+        //
+    }
+}
