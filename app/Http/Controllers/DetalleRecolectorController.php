9ad241a0d818408f890fc1a1c604f2d4e10388e5
fernandoDC <fernando@dcircuits.mx>
2020-10-14 18:33:43 -0500

    Controlador de detalle de recolectores agregado

---
+++ app/Http/Controllers/DetalleRecolectorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DeatalleRecolector;
+use Illuminate\Http\Request;
+
+class DetalleRecolectorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DeatalleRecolector  $deatalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DeatalleRecolector $deatalleRecolector)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DeatalleRecolector  $deatalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DeatalleRecolector $deatalleRecolector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DeatalleRecolector  $deatalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DeatalleRecolector $deatalleRecolector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DeatalleRecolector  $deatalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DeatalleRecolector $deatalleRecolector)
+    {
+        //
+    }
+}
