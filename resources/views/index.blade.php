1b988167cb8af2cdf53e2dc0684ab652aad87cb4
fernandoDC <fernando@dcircuits.mx>
2020-10-14 19:22:11 -0500

    Vista inicial agregada

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Puntos de Reciclaje</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+    <div class="d-flex p-2 bg-success">
+        <div class="p-2 ml-3 text-white"><a href="/" style="text-decoration:none;color:white;">Inicio</a></div>
+        <div class="p-2 ml-2 text-white"><a href="/" style="text-decoration:none;color:white;">Puntos de Reciclaje</a></div>
+        <div class="p-2 ml-2 text-white"><a href="/" style="text-decoration:none;color:white;">Recolectores</a></div>
+    </div>
+
+</body>
+</html>
